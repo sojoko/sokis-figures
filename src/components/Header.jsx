cc17c08c2e3d15a069abad7f5ea10c879c34f547
sojoko <jonhathansojo@gmail.com>
2024-05-20 18:52:30 -0500

    instrucctions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,8 +19,8 @@ function Header() {
       <a href="../">
         <p className='mt-4 lg:ml-16 text-center text-pink-500 text-3xl font-extrabold'>SOKIS FIGURES</p>
       </a>
-      <button onClick={openModal}>
-        <p className='mt-4 lg:mr-16 text-center text-pink-500 text-2xl font-extrabold'>INSTRUCCIONES</p>
+      <button onClick={openModal} className='bg-cyan-400 lg:mr-16 p-1 rounded-lg hover:bg-cyan-500 mt-2'>
+        <p className='mt-4text-center text-white text-2xl font-extrabold'>INSTRUCCIONES</p>
       </button>
       <ModalInstruc isOpen={isModalOpen} onClose={closeModal} />
     </div>
