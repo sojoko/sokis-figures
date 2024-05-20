bf68d6fff3741ea8b71e959a9b0d40f33d4fe7fc
sojoko <jonhathansojo@gmail.com>
2024-05-20 18:46:11 -0500

    instrucctions

--- src/components/ModalInstruc.jsx
+++ src/components/ModalInstruc.jsx
@@ -9,7 +9,7 @@ function ModalInstruc({ isOpen, onClose }) {
       <div className="bg-white p-6 rounded-lg shadow-lg z-10 max-w-md w-full">
         <h2 className="text-2xl font-bold mb-4 text-pink-500">Instrucciones</h2>
         <ol className="list-decimal pl-5 space-y-2">
-          <li className=''> <span className='text-cyan-600 font-bold'>Selecciona un tipo de figuras</span>
+          <li className=''> <span className='text-cyan-600 font-bold'>Selecciona un tipo de figura (planda o solida)</span>
             <ul className="list-disc pl-5">
               <li>Cada figura te permitirá calcular su área o volumen según sea el caso.</li>
             </ul>
@@ -19,7 +19,7 @@ function ModalInstruc({ isOpen, onClose }) {
               <li>Puedes hacer clic en una figura y luego introducir los valores según la fórmula dada. Al darle a guardar, se calculará el área o el volumen de la figura seleccionada.</li>
             </ul>
           </li>
-          <li className=''> <span className='text-cyan-600 font-bold'>Calcular figura irregular</span>
+          <li className=''> <span className='text-cyan-600 font-bold'>Calcula una figura irregular</span>
             <ul className="list-disc pl-5">
               <li>Una vez hayas terminado de calcular las figuras individuales, puedes hacer clic en "calcular figura irregular" para obtener la totalidad de todas las áreas/volúmenes.</li>
             </ul>
