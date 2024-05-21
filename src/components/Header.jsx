0b7847b48f93d0a97b86e4e72c83356f74c8faac
sojoko <jonhathansojo@gmail.com>
2024-05-20 19:02:32 -0500

    instrucctions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ function Header() {
         <p className='mt-4 lg:ml-16 text-center text-pink-500 text-3xl font-extrabold'>SOKIS FIGURES</p>
       </a>
       <button onClick={openModal} className='bg-cyan-400 lg:mr-16 p-1 rounded-lg hover:bg-cyan-500 mt-2'>
-        <p className='mt-4text-center text-white text-2xl font-extrabold'>INSTRUCCIONES</p>
+        <p className='mx-2 text-center text-white text-xl font-extrabold'>INSTRUCCIONES</p>
       </button>
       <ModalInstruc isOpen={isModalOpen} onClose={closeModal} />
     </div>
--- src/components/ModalInstruc.jsx
+++ src/components/ModalInstruc.jsx
@@ -31,7 +31,7 @@ function ModalInstruc({ isOpen, onClose }) {
           </li>
         </ol>
         <button
-          className="mt-6 bg-pink-500 text-white px-4 py-2 rounded"
+          className="mt-6 bg-pink-500 hover:bg-pink-400 text-white px-4 py-2 rounded"
           onClick={onClose}
         >
           Aceptar
